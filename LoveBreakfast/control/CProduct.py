bac092f12a71c2473e1b708cc0aa4521bc8d8099
shuaibinliu <13588046135@163.com>
2018-04-14 14:58:11 +0800

    增加前端把控参数Pnum 默认为0

--- LoveBreakfast/control/CProduct.py
+++ LoveBreakfast/control/CProduct.py
@@ -31,6 +31,7 @@ class CProduct():
             dic_of_pro["Pimage"] = pro_list_of_service[i].Pimage
             dic_of_pro["PsalesVolume"] = pro_list_of_service[i].P_sales_volume
             dic_of_pro["Pscore"] = pro_list_of_service[i].Pscore
+            dic_of_pro["Pnum"] = 0  # 前端控制用
             pro_list_of_control.append(dic_of_pro)
 
         print(pro_list_of_control)
