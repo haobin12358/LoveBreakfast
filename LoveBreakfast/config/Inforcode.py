be407cff8f6af41d6260e0faf003ec8eee9943c5
haobin12358 <1276121237@qq.com>
2018-05-03 15:11:27 +0800

    code delete

--- LoveBreakfast/config/Inforcode.py
+++
@@ -1,8 +0,0 @@
-# *- coding:utf8 *-
-# ACCESS_KEY_ID/ACCESS_KEY_SECRET 根据实际申请的账号信息进行替换
-ACCESS_KEY_ID = "LTAIRU515X6EotSQ"
-ACCESS_KEY_SECRET = "RkYdmk6QYVw4Oq8a14nCpU9o6olHh2"
-# 数字签名
-SignName = "etech研发团队"
-# 短信模板
-TemplateCode = "SMS_126866081"
