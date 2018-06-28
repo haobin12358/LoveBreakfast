4e630e3c6e832e64087d387307efa5e3a99e06cb
shuaibinliu <13588046135@163.com>
2018-06-28 10:04:31 +0800

    优惠券增加时间校验

--- LoveBreakfast/control/CCoupons.py
+++ LoveBreakfast/control/CCoupons.py
@@ -4,10 +4,11 @@ import os
 sys.path.append(os.path.dirname(os.getcwd()))
 from flask import request
 import json
+import datetime
 import uuid
 from common.get_model_return_list import get_model_return_list, get_model_return_dict
 from common.lovebreakfast_error import dberror
-from common.timeformate import get_db_time_str, get_web_time_str, format_forweb_no_HMS
+from common.timeformate import get_db_time_str, get_web_time_str, format_forweb_no_HMS, format_for_db
 from config.response import SYSTEM_ERROR, PARAMS_MISS
 from common.import_status import import_status
 
@@ -69,13 +70,14 @@ class CCoupons():
                 print(self.title.format("coupon"))
                 print(coupon)
                 print(self.title.format("coupon"))
+
                 coupon["COstart"] = get_web_time_str(coupon.get("COstart"), format_forweb_no_HMS)
                 coupon["COend"] = get_web_time_str(coupon.get("COend"), format_forweb_no_HMS)
                 cart_pkg["CAend"] = get_web_time_str(cart_pkg.get("CAend"), format_forweb_no_HMS)
                 cart_pkg["CAstart"] = get_web_time_str(cart_pkg.get("CAstart"), format_forweb_no_HMS)
-
                 cart_pkg.update(coupon)
-
+                if not self.check_carttime(cart_pkg):
+                    continue
                 cart_list.append(cart_pkg)
         except Exception as e:
             print("ERROR: " + e.message)
@@ -83,3 +85,19 @@ class CCoupons():
         result = import_status("messages_get_carpkg_success", "OK")
         result["data"] = cart_list
         return result
+
+    def check_carttime(self, cartpkg):
+        time_now = datetime.datetime.now().date()
+        if cartpkg.get("CAend") and \
+                time_now > datetime.datetime.strptime(cartpkg.get("CAend"), format_forweb_no_HMS).date():
+            return False
+        if cartpkg.get("CAstart") and \
+                time_now < datetime.datetime.strptime(cartpkg.get("CAstart"), format_forweb_no_HMS).date():
+            return False
+        if cartpkg.get("COend") and \
+                time_now > datetime.datetime.strptime(cartpkg.get("COend"), format_forweb_no_HMS).date():
+            return False
+        if cartpkg.get("COstart") and \
+                time_now < datetime.datetime.strptime(cartpkg.get("COstart"), format_forweb_no_HMS).date():
+            return False
+        return True
