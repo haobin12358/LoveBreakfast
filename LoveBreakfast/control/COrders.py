de85db2d6fb6fc3b54d2710671090f7bb200a44b
haobin12358 <1276121237@qq.com>
2018-06-29 14:59:21 +0800

    prnumber

--- LoveBreakfast/control/COrders.py
+++ LoveBreakfast/control/COrders.py
@@ -65,10 +65,10 @@ class COrders():
                 print(self.title.format("order_items"))
                 for raw in order_items:
                     Pid = raw.get("PRid")
-                    PRnum = raw.get("PRnum")
+                    PRnumber = raw.get("PRnumber")
                     product = get_model_return_dict(self.sproduct.get_product_all_by_pid(Pid))
                     product["PRid"] = Pid
-                    product["PRnum"] = PRnum
+                    product["PRnum"] = PRnumber
                     print(self.title.format("product"))
                     print(product)
                     print(self.title.format("product"))
@@ -126,7 +126,7 @@ class COrders():
             print(product)
             print(self.title.format("product"))
             product["PRid"] = row.get("PRid")
-            product["PRnum"] = row.get("PRnum")
+            product["PRnum"] = row.get("PRnumber")
             row.update(product)
 
         response_make_main_order = import_status("messages_get_item_ok", "OK")
